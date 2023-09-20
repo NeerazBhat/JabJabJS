87fda9aa13c2d40e5bd89580ba60d92bf0c26921
NeerazBhat <neeraz.bhat@gmail.com>
2023-09-20 12:28:37 +0545

    learn: js selectors

--- js/dom.js
+++ js/dom.js
@@ -10,4 +10,18 @@ buttonEl.setAttribute(
 );
 
 const ulEl = document.querySelector("ul");
-ulEl.innerHTML += `<li>There are ${ulEl.children.length} list</li>`;
+ulEl.innerHTML += `<li class="list">There are ${ulEl.children.length} list</li>`;
+
+const listEl = document.querySelectorAll('li');
+for(let list of listEl){
+  list.classList.add('list');
+}
+
+const listClass = document.getElementsByClassName('list');
+// const listClass = document.getElementsByTagName('li');
+console.log(listClass);
+
+// The QuerySelectorAll can select combination of classes, but it return static collection of node
+// getElementsByClassName can select one class name but it return HTMLcollection which is a live collection of elements
+
+
