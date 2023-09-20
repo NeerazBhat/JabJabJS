d77c2d522e0307d5cb23d90f409acaaf5db7d783
NeerazBhat <neeraz.bhat@gmail.com>
2023-09-20 14:11:23 +0545

    practise: toggle class exercise

--- js/dom.js
+++ js/dom.js
@@ -12,16 +12,21 @@ buttonEl.setAttribute(
 const ulEl = document.querySelector("ul");
 ulEl.innerHTML += `<li class="list">There are ${ulEl.children.length} list</li>`;
 
-const listEl = document.querySelectorAll('li');
-for(let list of listEl){
-  list.classList.add('list');
+const listEl = document.querySelectorAll("li");
+for (let list of listEl) {
+  list.classList.add("list");
 }
 
-const listClass = document.getElementsByClassName('list');
+const listClass = document.getElementsByClassName("list");
 // const listClass = document.getElementsByTagName('li');
 console.log(listClass);
 
 // The QuerySelectorAll can select combination of classes, but it return static collection of node
 // getElementsByClassName can select one class name but it return HTMLcollection which is a live collection of elements
 
+// Simple Toggle exercise
 
+buttonEl.addEventListener("click", () => {
+  newEl.classList.toggle("styled");
+  newEl.textContent = "This is changed now";
+});
